73897cf0984cf721ea4b9f7501bb56e9d3da2776
Jacob <jacob.m.mcfaul@gmail.com>
2021-04-13 15:54:19 -0600

    Gallery style change

--- twraps/src/components/ImgData.jsx
+++ twraps/src/components/ImgData.jsx
@@ -2,26 +2,17 @@ import jeep from "../img/jeep-wrap-1.png"
 import mustang from "../img/mustang-wrap-1.jpg"
 import truck from "../img/truck-wrap-1.jpeg"
 import porche from "../img/porche-wrap-1.jpeg"
+import kidsWall from "../img/wallwrap.png"
 
 export const Images = [
     { img: jeep, type: "Color", alt: "Jeep Color Wrap" },
     { img: mustang, type: "Commercial", alt: "Mustang Commercial Wrap" },
     { img: truck, type: "Commercial", alt: "Truck Commercial Wrap" },
     { img: porche, type: "Color", alt: "Porche Color Wrap" },
-    { img: jeep, type: "Color", alt: "Jeep Color Wrap" }
+    { img: jeep, type: "Color", alt: "Jeep Color Wrap" },
+    { img: kidsWall, type: "Wall", alt: "Animal Map" }
 ]
 
 export const CarouselImages = [
-    {
-        image: jeep
-    },
-    {
-        image: mustang
-    },
-    {
-        image: truck
-    },
-    {
-        image: porche
-    }
+    { image: jeep }, { image: mustang }, { image: truck }, { image: porche }
 ]
